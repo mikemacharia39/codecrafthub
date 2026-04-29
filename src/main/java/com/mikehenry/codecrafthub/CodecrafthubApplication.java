0621104fbe7d8203db03de1f4829c13ec7f3e49c
m_maina <m.maina@carepay.com>
2026-04-29 15:54:19 +0300

    first commit

---
+++ src/main/java/com/mikehenry/codecrafthub/CodecrafthubApplication.java
@@ -0,0 +1,13 @@
+package com.mikehenry.codecrafthub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CodecrafthubApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CodecrafthubApplication.class, args);
+	}
+
+}

aea194fc7aaae97af65a43685db4f0872eb361ad
m_maina <m.maina@carepay.com>
2026-04-29 17:52:39 +0300

    Build feature

---
+++ src/main/java/com/mikehenry/codecrafthub/configuration/JsonConfig.java
@@ -0,0 +1,24 @@
+package com.mikehenry.codecrafthub.configuration;
+
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.json.JsonMapper;
+import com.fasterxml.jackson.datatype.jsr310.JavaTimeModule;
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+
+import java.text.SimpleDateFormat;
+
+import static com.fasterxml.jackson.databind.DeserializationFeature.FAIL_ON_UNKNOWN_PROPERTIES;
+
+@Configuration
+public class JsonConfig {
+
+    @Bean
+    public ObjectMapper objectMapper() {
+        return JsonMapper.builder()
+                .addModules(new JavaTimeModule())
+                .disable(FAIL_ON_UNKNOWN_PROPERTIES)
+                .defaultDateFormat(new SimpleDateFormat("yyyy-MM-dd'T'HH:mm:ss"))
+                .build();
+    }
+}
